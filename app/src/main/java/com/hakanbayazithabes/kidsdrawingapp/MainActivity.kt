03d9b4258c46540330ee604568e9c812d08ea547
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-07-14 02:33:02 +0300

    Adding Permission requests

--- app/src/main/java/com/hakanbayazithabes/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/hakanbayazithabes/kidsdrawingapp/MainActivity.kt
@@ -1,18 +1,54 @@
 package com.hakanbayazithabes.kidsdrawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Message
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permisions ->
+            permisions.entries.forEach {
+                val permissionName = it.key //iznin ismi tutulur
+                val isGranted = it.value //izin verilip verilmediğine bakılır
+                print("Geldi")
+                if (isGranted) {
+                    Toast.makeText(
+                        this, "Permission granted now you can read the storage files",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this, "Oops you just denied the permission.", Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +71,11 @@ class MainActivity : AppCompatActivity() {
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
         }
     }
 
@@ -90,5 +131,63 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+    private fun requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                // Permission already granted
+                val pickIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                openGalleryLauncher.launch(pickIntent)
+            } else {
+                // Permission not granted, request it
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+                Toast.makeText(
+                    this,
+                    "Please grant permission to access external storage.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            // Android 10 or lower, request the permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                showRationaleDialog(
+                    "Drawing App",
+                    "Drawing App needs to Access your External Storage"
+                )
+            } else {
+                requestPermission.launch(
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                )
+            }
+        }
+    }
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackGround: ImageView = findViewById(R.id.iv_background)
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
 
 }
